2aa5b278b21a1735b31eaba6c6b8df04a8393dc2
twalk2 <2twalk2@gmail.com>
2019-11-14 14:41:50 -0700

    implemented axios call

--- src/components/BaseIcon.vue
+++ src/components/BaseIcon.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="icon-wrapper">
     <svg class="icon" :width="width" :height="height">
-      <use v-bind="{'xlink:href':'/feather-sprite.svg#'+name}"></use>
+      <use v-bind="{ 'xlink:href': '/feather-sprite.svg#' + name }"></use>
     </svg>
+    <slot></slot>
   </div>
 </template>
 
