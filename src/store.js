0ac998b4a17c2e0b0f5886ab1f667c1f7be49619
twalk2 <2twalk2@gmail.com>
2019-11-27 09:23:57 -0800

    vuex implementation

--- src/store.js
+++ src/store.js
@@ -4,8 +4,30 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    user: { id: 'abc123', name: 'Trevor' },
+    categories: [
+      'sustainability',
+      'nature',
+      'animal welfare',
+      'housing',
+      'education',
+      'food',
+      'community'
+    ]
+  },
   mutations: {},
   actions: {},
-  modules: {}
+  modules: {},
+  getters: {
+    catLength: state => {
+      return state.categories.length
+    },
+    doneTodo: state => {
+      return state.todos.filter(todo => todo.done)
+    },
+    activeTodosCount: state => {
+      return state.todos.filter(todo => !todo.done).length
+    }
+  }
 })
--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,3 +1,27 @@
 <template>
-  <h1>Create Event</h1>
+  <div>
+    <h1>Create Event, {{ user.name }}</h1>
+    <p>This event was created by {{ user.id }}</p>
+    <p>There are {{ catLength }} categories</p>
+    <ul>
+      <li v-for="cat in categories" :key="cat">{{ cat }}</li>
+    </ul>
+  </div>
 </template>
+
+<script>
+import { mapState } from 'vuex'
+export default {
+  computed: {
+    catLength() {
+      return this.$store.getters.catLength
+    },
+    ...mapState({
+      user: 'user',
+      categories: 'categories'
+    })
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
